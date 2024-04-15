d85cf99a678a74e6f3f7ac5e88dc93fac2038b6b
Fabricio Loayza <fabri.alexd@hotmail.com>
2024-04-15 16:35:43 -0500

    Se agrega el proyecto outliers

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ df[columnas_numericas] = df[columnas_numericas].replace({',': '.'}, regex=True)
 ## Graficar un diagrama de caja con seaborn
 # Esta línea crea una nueva figura de matplotlib con un tamaño de 10 pulgadas de ancho y 6 pulgadas de alto.
 # La función plt.figure() crea una nueva figura vacía, y el argumento figsize especifica las dimensiones de la figura.
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(20, 10))
 # Esta línea genera un diagrama de caja utilizando la biblioteca Seaborn.
 # Toma los datos del DataFrame df utilizando la función iloc para seleccionar todas las filas (:) y todas las columnas a partir de la segunda (1:),
 #  luego los convierte a tipo de dato float utilizando astype(float). Se crea un diagrama de caja para cada columna numérica en el DataFrame.
@@ -37,5 +37,7 @@ plt.xlabel('Estaciones de Ruido - Horario')
 plt.ylabel('Nivel de Ruido')
 # Esta línea rota las etiquetas del eje X 45 grados para mejorar la legibilidad si hay muchas estaciones de ruido.
 plt.xticks(rotation=45)
+# Guardar la figura como una imagen en la carpeta "output"
+plt.savefig('C:/Users/fabri/Documents/workspace/Analisis_de_outliers/output/diagrama_caja_ruido.png')
 # Esta línea muestra la figura completa con todas las características especificadas anteriormente.
 plt.show()
